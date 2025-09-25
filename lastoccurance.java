4da7aeeea8289143345110e6b1d4410aa4204e27
Satyam Kumar <satyamkumarsingh8876@gmail.com>
2025-09-26 01:26:48 +0530

    Add files via upload

---
+++ lastoccurance.java
@@ -0,0 +1,24 @@
+public class lastoccurance {
+    public static int lastoccuranceelement(int arr[], int k){
+        int start=0;
+        int end=arr.length-1;
+        int ans=-1;
+        while(start<=end){
+            int mid=(start+(end-start)/2);
+            if(arr[mid]==k){
+                ans=mid;
+                start=mid+1;
+            }else if(arr[mid]>k){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1, 1, 1, 2, 2, 3, 3, 3, 4, 5};
+        System.out.println(lastoccuranceelement(arr, 3));
+    }
+}
